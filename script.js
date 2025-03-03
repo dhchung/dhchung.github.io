525bf51e81c21b8cf28cc92479c10609584a876b
Dongha Chung <haetae.dongha@gmail.com>
2025-03-03 20:28:37 +0900

    include swiper

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setupSlider();
-});
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 300,
+    slidesPerView: 1,
+    spaceBetween: 0,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
 
-// 이미지 슬라이더 스와이프 기능
-let currentIndex = 0;
-const slider = document.getElementById("slider");
-let startX = 0;
-let isDragging = false;
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-slider.addEventListener("touchstart", function (e) {
-    startX = e.touches[0].clientX;
-    isDragging = true;
-});
-
-slider.addEventListener("touchmove", function (e) {
-    if (!isDragging) return;
-    let moveX = e.touches[0].clientX - startX;
-    slider.style.transform = `translateX(${moveX - currentIndex * 100}%)`;
-});
-
-slider.addEventListener("touchend", function (e) {
-    isDragging = false;
-    let endX = e.changedTouches[0].clientX;
-    let diff = startX - endX;
-    if (diff > 50) {
-        nextSlide();
-    } else if (diff < -50) {
-        prevSlide();
-    } else {
-        showSlide(currentIndex);
-    }
-});
-
-function showSlide(index) {
-    slider.style.transform = `translateX(${-index * 100}%)`;
-}
-
-function prevSlide() {
-    currentIndex = Math.max(0, currentIndex - 1);
-    showSlide(currentIndex);
-}
-
-function nextSlide() {
-    currentIndex = Math.min(slider.children.length - 1, currentIndex + 1);
-    showSlide(currentIndex);
-}
-
-function setupSlider() {
-    showSlide(currentIndex);
-}
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+  });
